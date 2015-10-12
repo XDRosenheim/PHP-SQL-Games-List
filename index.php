0cc9cd33dc63ad46cf697399a9fc366b5be8d661
XDRosenheim <patrickrosenheim@gmail.com>
2015-10-12 14:43:46 +0200

    Fixing

--- index.php
+++ index.php
@@ -11,38 +11,14 @@
 
 <!DOCTYPE html>
 <html>
-  <?php
-    include("headerItems.php");
-  ?>
 <head>
-  <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/font-awesome.min.css">
-  <link rel="stylesheet" href="css/style.css">
-  <!-- Latest compiled and minified JavaScript -->
-  <script src="js/jquery-2.1.4.min.js"></script>
-  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <?php
+    include_once("headerItems.php");
+  ?>
 </head>
 
 <body>
   <ul class="nav navbar-nav">
-    <!-- Rust -->
-    <li class="dropdown">
-      <a class="dropdown-toggle" data-toggle="dropdown">Rust <i class="fa fa-arrow-down"></i></a>
-      <ul class="dropdown-menu" role="menu">
-        <li class="dropdown-header">Fanmade wiki</li>
-        <li><a href="http://rust.wikia.com/wiki/Rust_Wiki">Wikia</a></li>
-        <li class="divider"></li>
-        <li class="dropdown-header">Steam links</li>
-        <li><a href="http://store.steampowered.com/app/252490">Store</a></li>
-        <li><a href="http://steamcommunity.com/app/252490">Community</a></li>
-        <li><a href="http://steamcommunity.com/app/252490/screenshots">Screenshots</a></li>
-        <li><a href="https://steamcommunity.com/workshop/browse?appid=252490">Workshop</a></li>
-        <li><a href="http://steamcommunity.com/app/252490/guides">Guides</a></li>
-        <li><a href="http://steamcommunity.com/app/252490/allnews">News</a></li>
-        <li><a href="http://steamcommunity.com/app/252490/discussions">Discussions</a></li>
-      </ul>
-    </li>
     <?php
       for ( $i = 0; $i < $gamesListLength; $i++ ) {
         if ( $gamesList[$i][3] ) {
@@ -51,7 +27,7 @@
           $wikiString = "Fanmade wiki";
         };
         print "<li class=\"dropdown\">";
-        print "<a class=\"dropdown-toggle\" data-toggle=\"dropdown\">Rust <i class=\"fa fa-arrow-down\"></i></a>";
+        print "<a class=\"dropdown-toggle\" data-toggle=\"dropdown\">" . $gamesList[$i][0] . "<i class=\"fa fa-arrow-down\"></i></a>";
         print "<ul class=\"dropdown-menu\" role=\"menu\">";
         print "<li class=\"dropdown-header\">" . $wikiString . "</li>";
         print "<li><a href=\"" . $gamesList[$i][4] . "\">Wiki</a></li>";
